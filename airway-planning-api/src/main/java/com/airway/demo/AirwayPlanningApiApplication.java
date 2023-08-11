c0e65e9cf6dfd74779948062bd48f17618c8b0f7
Your Name <youremail@example.com>
2023-08-11 18:03:19 +0100

    Create Spring Boot project with dependencies
    set up the initial structure of the Spring Boot project.
    I've added necessary dependencies and configuration to the pom.xml file
    to set up the project for further development.

---
+++ airway-planning-api/src/main/java/com/airway/demo/AirwayPlanningApiApplication.java
@@ -0,0 +1,13 @@
+package com.airway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwayPlanningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwayPlanningApiApplication.class, args);
+	}
+
+}
---
+++ airway-planning-api/src/test/java/com/airway/demo/AirwayPlanningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwayPlanningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
